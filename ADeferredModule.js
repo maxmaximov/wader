4a99e997fa4e5cb804abf225fafc3decdf897627
Max Maximov <max.maximov@gmail.com>
2012-02-08 02:56:50 +0400

    Убрал тестовый мусор

--- ADeferredModule.js
+++ ADeferredModule.js
@@ -4,7 +4,6 @@
  * @author Max Maximov <max.maximov@gmail.com>
  * @version 0.2.1
  */
-// Тестовый комит
 define("app/ADeferredModule", ["app/Hub", "app/Logger", "app/IModule"], function(Hub, Logger, IModule) {
     "use strict";
 
