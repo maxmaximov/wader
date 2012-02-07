ca21e3d8b120e584adcb990c8baa54d398c78f11
Max Maximov <max.maximov@gmail.com>
2012-02-08 01:50:37 +0400

    Тестовый комит

--- app/ADeferredModule.js
+++ app/ADeferredModule.js
@@ -4,6 +4,7 @@
  * @author Max Maximov <max.maximov@gmail.com>
  * @version 0.2.1
  */
+// Тестовый комит
 define("app/ADeferredModule", ["app/Hub", "app/Logger", "app/IModule"], function(Hub, Logger, IModule) {
     "use strict";
 
