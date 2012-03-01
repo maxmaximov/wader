290f6cdb65f6d833dc2614076411b20ce8b63b7f
Max Maximov <max.maximov@gmail.com>
2012-03-01 22:46:50 +0400

    IComposite & AComposite

---
+++ AComposite.js
@@ -0,0 +1,57 @@
+/**
+ * App Composite Abstract Class
+ *
+ * @author Max Maximov <max.maximov@gmail.com>
+ * @version 0.2.1
+ */
+define("app/AComposite", ["app/Hub", "app/Logger", "app/IComposite"], function(Hub, Logger, IComposite) {
+    "use strict";
+
+    /*
+    * @class app.AComposite
+    * @abstract
+    * @implements app.IComposite
+    */
+    IComposite.extend("app.AComposite",
+
+    /* @Static */
+    {
+    },
+
+    /* @Prototype */
+    {
+        _children: [],
+
+        /*
+        * @param {<Child>} child
+        * @return undefined
+        */
+        add: function (child) {
+            this._children.push(child);
+        },
+
+        /*
+        * @param {<Child>} child
+        * @return undefined
+        */
+        remove: function (child) {
+            for (var i = 0, len = this._children.length; i < len; i++) {
+                if (this._children[i] === child) {
+                    this._children.splice(i, 1);
+                    break;
+                }
+            }
+        },
+
+        /*
+        * @param {number} i
+        * @return <Child>
+        */
+        getChild: function (i) {
+            return this._children[i];
+        }
+    });
+
+    return app.AComposite;
+});
+
---
+++ IComposite.js
@@ -0,0 +1,40 @@
+/**
+ * App Composite Interface
+ *
+ * @author Max Maximov <max.maximov@gmail.com>
+ * @version 0.2.1
+ */
+define("app/IComposite", ["app/Hub", "app/Logger"], function (Hub, Logger) {
+    "use strict";
+
+    /*
+    * @interface app.IComposite
+    */
+    $.Class.extend("app.IComposite",
+
+    /* @Static */
+    {
+    },
+
+    /* @Prototype */
+    {
+        init: function () {
+            throw new Error(this.constructor.fullName + ": не реализован метод init");
+        },
+
+        add: function () {
+            throw new Error(this.constructor.fullName + ": не реализован метод add");
+        },
+
+        remove: function () {
+            throw new Error(this.constructor.fullName + ": не реализован метод remove");
+        },
+
+        getChild: function () {
+            throw new Error(this.constructor.fullName + ": не реализован метод getChild");
+        }
+    });
+
+    return app.IComposite;
+});
+
