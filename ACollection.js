e207f851bb93ac55047c258bbe4e0f183ad9cab8
Max Maximov <max.maximov@gmail.com>
2012-08-15 21:32:16 +0400

    Recursive toArray

--- ACollection.js
+++ ACollection.js
@@ -127,11 +127,11 @@
             return items;
         },
 
-        toArray: function () {
+        toArray: function (recursive) {
             var items = [];
             for (var i = 0, l = this._items.length; i < l; i++) {
                 if (this._items[i] && !this._items[i].isDeleted() && !this._items[i].isNew()) {
-                    items[i] = this._items[i].toArray();
+                    items[i] = this._items[i].toArray(recursive);
                 }
             }
 
